6a6c53c50ac6c72786e2b4fba98610c3cadd647d
DevNap <thdwodh789@gmail.com>
2025-04-28 14:25:33 +0900

    [Feat] 플레이어 움직임 구현

--- Assets/SJO/PlayerMove.cs
+++ Assets/SJO/PlayerMove.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 플레이어 이동속도를 위한 변수
+    [SerializeField] float _moveSpeed;
+    [SerializeField] Rigidbody rigid;
+
+    private Vector3 userInputVec;
+
+    private void FixedUpdate()
     {
-        
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        userInputVec = new Vector3(x, 0, z).normalized;
+    }
+
+    private void Move()
+    {
+        rigid.velocity = userInputVec * _moveSpeed;
     }
 }
