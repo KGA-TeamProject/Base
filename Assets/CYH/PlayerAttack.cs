3567c5da486dc75dad3c580225bfc35ada6275fc
YeonHouu <yeonhhou@gmail.com>
2025-04-28 19:44:45 +0900

    [Chore] 프로젝트 세팅
    - UI 사용을 위한 TMPro 설치
    - 플레이어 공격 구현을 위한 스크립트와 씬 생성

---
+++ Assets/CYH/PlayerAttack.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+   // 플레이어 공격
+}
---
+++ Assets/CYH/Projectile.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+   //발사체별 필수 속성
+}
