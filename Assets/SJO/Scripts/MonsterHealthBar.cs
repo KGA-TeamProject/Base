5e6bf03c0e0593105d454104e48a5a17cfb77eba
DevNap <thdwodh789@gmail.com>
2025-04-29 16:11:56 +0900

    [Feat] 몬스터 체력바 기능 구현

--- Assets/SJO/Scripts/MonsterHealthBar.cs
+++ Assets/SJO/Scripts/MonsterHealthBar.cs
@@ -5,5 +5,33 @@ using UnityEngine.UIElements;
 
 public class MonsterHealthBar : MonoBehaviour
 {
-    
+    [SerializeField] Slider hpBar;
+
+    private int hp;
+
+    public int monsterHP
+    {
+        get => hp;
+
+        private set => hp = Mathf.Clamp(value, 0, hp);
+    }
+
+    private void Awake()
+    {
+        hp = 100;
+        SetMaxHealth(hp);
+    }
+
+    public void SetMaxHealth(int monsterHealth)
+    {
+        // hpBar.maxValue = monsterHealth;
+        hpBar.value = monsterHealth;
+    }
+
+    public void TakenDamage(int damage)
+    {
+        int damageTaken = monsterHP - damage;
+        monsterHP = damageTaken;
+        hpBar.value = monsterHP;
+    }
 }
