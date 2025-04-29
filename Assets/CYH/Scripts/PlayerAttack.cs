cf1f442a12533fdfaec6a6621d21cc9bb09ccfd6
YeonHouu <yeonhhou@gmail.com>
2025-04-29 14:37:04 +0900

    [Feat] 발사체 개수에 따른 각도 조절 발사 기능 추가
    - 공격 스킬 더블샷, 트리플샷 연출을 위한 발사체 개수 별 각도 조절 발사 기능 구현

--- Assets/CYH/Scripts/PlayerAttack.cs
+++ Assets/CYH/Scripts/PlayerAttack.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-   // 플레이어 공격
+    public Projectile prefab;
+    public int shootCount;
+    public float angle = 15;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        // 발사 총알 개수에 따른 발사 각도 조절
+        for (int i = 0; i < shootCount; i++)
+        {
+            float arrowAngle = -angle / 2 + angle / (shootCount + 1) * (i + 1);
+            Quaternion arrowRotation = Quaternion.Euler(0, arrowAngle, 0);
+
+            Projectile instance = Instantiate(prefab, transform.position, transform.rotation * arrowRotation);
+            instance.rigid.velocity = instance.transform.forward * instance.speed;
+        }
+    }
 }
--- Assets/CYH/Scripts/Projectile.cs
+++ Assets/CYH/Scripts/Projectile.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-   //발사체별 필수 속성
+    //발사체별 필수 속성
+
+    [SerializeField] public int damage;
+    [SerializeField] public int speed;
+    public Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
 }
