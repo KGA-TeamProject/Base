5e7b724f638280b754ff961cebe58a6561045ef7
Lazism2j <lazism2j@gmail.com>
2025-04-29 12:09:10 +0900

    [Fix] 몬스터 rotate방식 수정
    움직임이 부자연스러워 Quaternion.Lerp에서 LookAt으로 수정

--- Assets/LJ2/monsterController.cs
+++ Assets/LJ2/monsterController.cs
@@ -6,20 +6,11 @@ public class monsterController : MonoBehaviour
 {
     [SerializeField] private int Hp;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float rotateInterpolation;
-    
-
-
-
     
 
     public void Move(Transform target)
     {
-        transform.rotation = Quaternion.Lerp(
-            transform.rotation,
-            Quaternion.LookRotation(target.position),
-            rotateInterpolation * Time.deltaTime
-            );
+        transform.LookAt(target);
 
         transform.position = Vector3.MoveTowards(
             transform.position,
