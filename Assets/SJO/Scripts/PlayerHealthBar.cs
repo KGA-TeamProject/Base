07300848fba6447fd1d0a727b244424b106f29fc
DevNap <thdwodh789@gmail.com>
2025-04-29 15:36:24 +0900

    [Feat] 플레이어 데미지 바 구현
    플레이어가 입은 데미지 만큼 체력바가 줄어들게 하기 위한 기능 구현.

--- Assets/SJO/Scripts/PlayerHealthBar.cs
+++ Assets/SJO/Scripts/PlayerHealthBar.cs
@@ -9,12 +9,12 @@ public class PlayerHealthBar : MonoBehaviour
 
     private int hp;
 
-    private int playerHP
+    public int playerHP
     {
         // get으로 PlayerHealthBar 이외에 접근하지 못하도록 설정
         get => hp;
         // 최소 / 최대값 설정으로 int가 0의 범위를 넘지 않도록 설정
-        set => hp = Mathf.Clamp(value, 0, hp);
+        private set => hp = Mathf.Clamp(value, 0, hp);
     }
 
     // Awake 단계에서 초기 세팅
@@ -24,8 +24,18 @@ public class PlayerHealthBar : MonoBehaviour
         hp = 100;
     }
 
-    private void SetMaxHealth()
+    // 체력 최대값 설정을 위한 함수 세팅
+    public void SetMaxHealth(int playerHealth)
     {
+        // hpBar.maxValue = playerHealth;
+        hpBar.value = playerHealth;
+    }
 
+    // 몬스터의 공격력 만큼 HP에 할당
+    public void TakenDamage(int damage)
+    {
+        int damageTaken = playerHP - damage;
+        playerHP = damageTaken;
+        hpBar.value = playerHP;
     }
 }
