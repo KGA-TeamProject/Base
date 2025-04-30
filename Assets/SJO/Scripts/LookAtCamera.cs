2afa11fd6ba5a441c21ab5cfcd0a4fe98a541d5f
DevNap <thdwodh789@gmail.com>
2025-04-30 16:41:04 +0900

    [Refactor] 체력바가 카메라를 바라보도록 구현
    빌드업을 통해 체력바가 카메라를 바라보도록 설정

--- Assets/SJO/Scripts/LookAtCamera.cs
+++ Assets/SJO/Scripts/LookAtCamera.cs
@@ -6,8 +6,22 @@ public class LookAtCamera : MonoBehaviour
 {
     // 체력바가 메인 카메라 바라보게 하기
 
+    // 빌드업 사용
+    Transform camera;
+
+    private void Start()
+    {
+        // 시작시 카메라의 위치를 메인 카메라의 위치로
+        camera = Camera.main.transform;
+    }
+
+    // LateUpdate에서 체력바가 카메라를 바라보도록
     private void LateUpdate()
     {
-        transform.forward = Camera.main.transform.forward;
+        transform.LookAt(transform.position + camera.rotation * Vector3.forward, camera.rotation * Vector3.up);
+
+        // 이렇게만 적어도 상관없음
+        // transform.forward = Camera.main.transform.forward;
     }
+
 }
