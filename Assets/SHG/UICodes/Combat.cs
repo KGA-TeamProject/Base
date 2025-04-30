61d0a170bf326a4d22c743fd0cf21835c46c8144
bart <bartshin@icloud.com>
2025-04-30 15:14:41 +0900

    [Feat] 미니맵 줌 기능

--- Assets/SHG/UICodes/Combat.cs
+++ Assets/SHG/UICodes/Combat.cs
@@ -11,19 +11,19 @@ public class CombatUI : MonoBehaviour
   const string CONTAINER_NAME = "combatUI_container";
   Minimap minimap;
   VisualElement root;
-  Camera minimapCamera;
   Transform playerTransform;
+  Coroutine zoomMinimapRoutine;
 
   void Awake()
   {
     this.Init();
-    this.minimapCamera = GameObject.Find("Minimap Camera").GetComponent<Camera>();
   }
   // Start is called before the first frame update
   void Start()
   {
     this.playerTransform = GameObject.FindWithTag("Player").transform;
     this.Show();
+    this.ZoomMinimap();
   }
 
   // Update is called once per frame
@@ -31,13 +31,14 @@ public class CombatUI : MonoBehaviour
   {
     if (GameManager.Shared.State == GameManager.GameState.InCombat &&
         GameManager.Shared.IsPlaying) {
-      this.MinimapCameraFollowPlayer();
+      this.UpdateMinimap();
     }
   }
 
   void Init()
   {
     this.minimap = new();
+    this.minimap.Camera = GameObject.Find("Minimap Camera").GetComponent<Camera>();
     this.root = this.GetComponent<UIDocument>().rootVisualElement;
     this.root.AddToClassList(CombatUI.CONTAINER_NAME);
     this.root.Add(this.minimap);
@@ -55,17 +56,25 @@ public class CombatUI : MonoBehaviour
     this.IsShowing = false;
   }
 
-  void MinimapCameraFollowPlayer()
+  void ZoomMinimap()
+  {
+    if (this.zoomMinimapRoutine != null) {
+      this.StopCoroutine(this.zoomMinimapRoutine);
+    }
+    this.zoomMinimapRoutine = this.StartCoroutine(
+        this.minimap.Zoom(10f, () => this.zoomMinimapRoutine = null)
+        );
+  }
+
+  void UpdateMinimap()
   {
     var newPos = new Vector3(
         this.playerTransform.position.x,
-        this.minimapCamera.transform.position.y,
+        this.minimap.Camera.transform.position.y,
         this.playerTransform.position.z
         );
-    this.minimapCamera.transform.position = newPos;
-    this.minimapCamera.transform.LookAt(this.playerTransform.transform.position);
+    this.minimap.MoveCameraCenterTo(newPos);
   }
-
 }
 
 public interface IUIComponent 
--- Assets/SHG/UICodes/Minimap.cs
+++ Assets/SHG/UICodes/Minimap.cs
@@ -1,15 +1,38 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Minimap : VisualElement, IUIComponent
 {
+  public Camera Camera;
   public const string CONTAINER_NAME = "minimap_container";
   RenderTexture renderTexture;
+  public float ZoomLerp = 0.5f;
 
   public Minimap()
   {
     this.Init();
     this.CreateUI();
+  }
+
+  public void MoveCameraCenterTo(Vector3 pos)
+  {
+    this.Camera.transform.position = pos;
+    this.Camera.transform.LookAt(pos);
+  }
+
+  public IEnumerator Zoom(float size, Action callback = null)
+  {
+    while (Mathf.Abs(this.Camera.orthographicSize - size) > float.Epsilon) {
+      this.Camera.orthographicSize = Mathf.Lerp(
+          this.Camera.orthographicSize,
+          size,
+          this.ZoomLerp * Time.deltaTime
+          );
+      yield return (null);
+    }
+    callback?.Invoke();
   }
 
   void Init()
@@ -22,4 +45,5 @@ public class Minimap : VisualElement, IUIComponent
     this.name = Minimap.CONTAINER_NAME;
     this.style.backgroundImage = new StyleBackground(Background.FromRenderTexture(this.renderTexture));
   }
+
 }
