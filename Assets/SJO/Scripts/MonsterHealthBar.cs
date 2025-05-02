039e646609ac464ffbcbbc66609e54f9f92f4f88
DevNap <thdwodh789@gmail.com>
2025-05-02 10:53:13 +0900

    [Refactor] 몬스터 체력바 코드 수정

--- Assets/SJO/Scripts/MonsterHealthBar.cs
+++ Assets/SJO/Scripts/MonsterHealthBar.cs
@@ -7,31 +7,48 @@ public class MonsterHealthBar : MonoBehaviour
 {
     [SerializeField] Slider hpBar;
 
-    private int hp;
+    // 체력을 알 수 있는 변수 생성
+    public float currentHP;
+    public float maxHP;
 
-    public int monsterHP
+    // 현재 체력과 최대 체력을 연결
+    public void SetUpHP(float amount)
     {
-        get => hp;
-
-        private set => hp = Mathf.Clamp(value, 0, hp);
+        maxHP = amount;
+        currentHP = maxHP;
     }
 
-    private void Awake()
+    // 체력을 체크 할 함수
+    public void CheckHP()
     {
-        hp = 100;
-        SetMaxHealth(hp);
+        // 만약 체력바가 null이 아니면
+        if (hpBar != null)
+        {
+            // 체력바 값을 현재 체력 나누기 최대 체력으로
+            hpBar.value = (float)currentHP / (float)maxHP;
+        }
     }
 
-    public void SetMaxHealth(int monsterHealth)
+    // 데미지를 받을 수 있는 함수
+    public void TakeDamage(float damageTaken)
     {
-        // hpBar.maxValue = monsterHealth;
-        hpBar.value = monsterHealth;
+        // 만약 최대 체력과 현재 체력이 0이거나 0보다 작으면 return
+        if (maxHP == 0 || currentHP <= 0)
+        {
+            return;
+        }
+        // 현재 체력에서 받은 데미지 차감
+        currentHP -= damageTaken;
+
+        if (currentHP <= 0)
+        {
+            // 몬스터 사망
+            Die();
+        }
     }
 
-    public void TakenDamage(int damage)
+    private void Die()
     {
-        int damageTaken = monsterHP - damage;
-        monsterHP = damageTaken;
-        hpBar.value = monsterHP;
+        Destroy(gameObject);
     }
 }
